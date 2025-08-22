516edf0d6cf9534c959a78e4db0a8060c8e60371
Mykhailo <miskasalyuta@gmail.com>
2025-08-22 17:18:14 +0300

    feat(car): add Car model with unique license_plate and M2M drivers

--- taxi/models.py
+++ taxi/models.py
@@ -1,5 +1,20 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 class Driver(AbstractUser):
     pass
+
+
+class Car(models.Model):
+    manufacturer = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    license_plate = models.CharField(max_length=20, unique=True)
+    drivers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="cars",
+        blank=True
+    )
+
+    def __str__(self):
+        return f"{self.manufacturer} {self.model} ({self.license_plate})"
