a933c4fc521b4d968d9ff6202063eb4dbb62f8ed
Mykhailo <miskasalyuta@gmail.com>
2025-08-22 17:23:46 +0300

    feat(admin): register Car with admin (filters/search)

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Driver
+from .models import Driver, Car
 
 @admin.register(Driver)
 class DriverAdmin(admin.ModelAdmin):
     list_display = ("id", "username", "email", "is_active", "is_staff", "date_joined")
     search_fields = ("username", "email")
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ("id", "manufacturer", "model", "license_plate")
+    search_fields = ("manufacturer", "model", "license_plate")
+    filter_horizontal = ("drivers",)
