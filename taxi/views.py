d9e226300afe6e38ab58001e69eae2125d991a66
Mykhailo <miskasalyuta@gmail.com>
2025-08-22 17:21:56 +0300

    feat(api): add Car serializer and viewset

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Car
+from .serializers import CarSerializer
 
-# Create your views here.
+class CarViewSet(viewsets.ModelViewSet):
+    queryset = Car.objects.all().order_by("id")
+    serializer_class = CarSerializer
+    permission_classes = [permissions.AllowAny]
