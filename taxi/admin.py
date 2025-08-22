ae39ea066fb02c3190bb037a5d29e0ed092a0eac
Mykhailo <miskasalyuta@gmail.com>
2025-08-22 17:14:32 +0300

    feat(admin): register Driver in admin site

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Driver
 
-# Register your models here.
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "email", "is_active", "is_staff", "date_joined")
+    search_fields = ("username", "email")
